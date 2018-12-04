b8af6cd635396f3a474313fb55de7114a5d2df3c
Nathan Johnson <njohnson@ena.com>
2018-12-03 21:17:19 -0600

    improved 0301.go and 0102.go

--- 0102/0102.go
+++ 0102/0102.go
@@ -12,12 +12,13 @@ func main() {
 
 	totals := make(map[int]bool)
 	total := 0
-	for outer := 0; func() bool {
-		fh, err := os.Open("./input.txt")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer fh.Close()
+	fh, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fh.Close()
+outside:
+	for outer := 0; ; outer++ {
 		reader := bufio.NewScanner(fh)
 		for i := 0; reader.Scan(); i++ {
 			t := reader.Text()
@@ -29,12 +30,14 @@ func main() {
 			total += nval
 			if totals[total] {
 				fmt.Printf("found total %d on outer %d inner %d\n", total, outer, i)
-				return false
+				break outside
 			}
 			totals[total] = true
 		}
-		return true
-	}(); outer++ {
+		_, err := fh.Seek(0, 0)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	fmt.Printf("total: %d\n", total)
--- 0301/0301.go
+++ 0301/0301.go
@@ -5,9 +5,16 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
 	"strconv"
 )
+
+type rec struct {
+	id uint16
+	ox uint16
+	oy uint16
+	dx uint16
+	dy uint16
+}
 
 func main() {
 	fh, err := os.Open("./input.txt")
@@ -17,32 +24,37 @@ func main() {
 
 	defer fh.Close()
 	reader := bufio.NewScanner(fh)
-	re := regexp.MustCompile(`^#(\d+)\s+@\s+(\d+),(\d+):\s+(\d+)x(\d+)$`)
-	var grid [1000][1000]int
-	for lineNo := 0; reader.Scan(); lineNo++ {
-		matches := re.FindStringSubmatch(reader.Text())
-		if len(matches) == 6 {
-			ox := cvt(matches[2])
-			oy := cvt(matches[3])
-			dx := cvt(matches[4])
-			dy := cvt(matches[5])
-			for x := ox; x < ox+dx; x++ {
-				for y := oy; y < oy+dy; y++ {
-					grid[x][y]++
-				}
-			}
-		}
-	}
+	var grid [1000][1000]uint8
 	overlapInches := 0
-	for x := 0; x < 1000; x++ {
-		for y := 0; y < 1000; y++ {
-			if grid[x][y] > 1 {
-				overlapInches++
+
+	for lineNo := 0; reader.Scan(); lineNo++ {
+		r := parse(reader.Text())
+		for x := r.ox; x < r.ox+r.dx; x++ {
+			for y := r.oy; y < r.oy+r.dy; y++ {
+				grid[x][y]++
+				if grid[x][y] == 2 {
+					overlapInches++
+				}
 			}
 		}
 	}
 
 	fmt.Printf("overlap inches: %d\n", overlapInches)
+}
+
+func parse(line string) rec {
+	r := rec{}
+	_, err := fmt.Sscanf(line, "#%d @ %d,%d: %dx%d",
+		&r.id,
+		&r.ox,
+		&r.oy,
+		&r.dx,
+		&r.dy,
+	)
+	if err != nil {
+		panic(fmt.Sprintf("%s %s", line, err))
+	}
+	return r
 }
 
 func cvt(s string) int {
