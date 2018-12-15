daddc32f617702274195cf9639227ae9cbe226ea
Nathan Johnson <njohnson@ena.com>
2018-12-14 20:15:56 -0600

    1402 - IT'S UNDER 600!

--- 1402/1402.go
+++ 1402/1402.go
@@ -12,50 +12,45 @@ import (
 func main() {
 
 	var input string
+	var capSize uint
 	flag.StringVar(&input, "input", "157901", "puzzle input")
+	flag.UintVar(&capSize, "capsize", 25000000, "tweak the slice cap")
 	flag.Parse()
-	tmp, err := strconv.ParseUint(input, 10, 32)
+	_, err := strconv.ParseUint(input, 10, 32)
 	if err != nil {
-		log.Fatalf("not numeric input")
+		log.Fatalf("input is not positive integer: %s", err)
 	}
-	bigLine := make([]byte, 2, tmp*150)
+	bigLine := make([]byte, 2, capSize)
 
-	elf1 := 0
-	elf2 := 1
-
-	bigLine[elf1] = 3
-	bigLine[elf2] = 7
+	elf1, elf2 := 0, 1
+	bigLine[elf1], bigLine[elf2] = 3, 7
 
 	sequence := []byte(input)
-	for i, d := range sequence {
-		sequence[i] = d - '0'
+	for i := range sequence {
+		sequence[i] -= '0'
 	}
 	t := time.Now()
-	for !HasSuffix(bigLine, sequence) {
-		bigLine = append(bigLine, Itobs(int(bigLine[elf1]+bigLine[elf2]))...)
+OUTER:
+	for {
+		for _, d := range digitSlice(uint(bigLine[elf1] + bigLine[elf2])) {
+			bigLine = append(bigLine, d)
+			if bytes.HasSuffix(bigLine, sequence) {
+				break OUTER
+			}
+		}
 		elf1 += int(bigLine[elf1]) + 1
 		elf1 %= len(bigLine)
 		elf2 += int(bigLine[elf2]) + 1
 		elf2 %= len(bigLine)
 	}
 	elapsed := time.Since(t)
-	fmt.Printf("%s first appears after %d recipes : %s\n", input, bytes.LastIndex(bigLine, sequence), elapsed)
+	fmt.Printf("%s first appears after %d recipes : %s\n", input, len(bigLine)-len(sequence), elapsed)
+
 }
 
-func Itobs(i int) []byte {
-	if i < 10 {
-		return []byte{byte(i)}
+func digitSlice(ui uint) []byte {
+	if ui < 10 {
+		return []byte{byte(ui)}
 	}
-	return []byte{byte(i / 10), byte(i % 10)}
-}
-
-func HasSuffix(s, suffix []byte) bool {
-	n := len(s)
-	sl := len(suffix)
-	switch {
-	case n > sl+1:
-		return bytes.Contains(s[n-sl-1:], suffix)
-	default:
-		return bytes.HasSuffix(s, suffix)
-	}
+	return []byte{byte(ui / 10), byte(ui % 10)}
 }
